b5656ac1f508a91bb8c4e2466ac6de234ba675a4
Zvonimir <zvonimir.konjevic@gmail.com>
2023-01-02 00:28:54 +0100

    Create function createGrid()
    Created function called createGrid() that creates grid that has dimension of grid
    as argument of function and based on that arguments calculates size of each
    square's edges and sets number of rows and columns then appends rows*columns
    number of squares to div with class container.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 let container = document.querySelector('.container');
 
 let uiHeight = container.offsetHeight;
+
+function createGrid(dimension){
+    let squareEdge = uiHeight / dimension;
+    container.style.setProperty('--grid-rows', dimension);
+    container.style.setProperty('--grid-cols', dimension);
+    container.style.setProperty('--grid-size', squareEdge + 'px');
+
+    for(let i = 0; i < dimension*dimension; i++){
+        let square = document.createElement('div');
+        container.appendChild(square).className = 'grid-item';
+    }
+}
+
+createGrid(16);
