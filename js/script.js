a98445ce4f97c0cf7071a5a59db1492aa3b24f17
Zvonimir <zvonimir.konjevic@gmail.com>
2023-01-05 16:33:40 +0100

    Rework function drawOnHover()
    Reworked function drawOnHover() by adding else if statement that checks if ALT
    key is being pressed and then depending if it's true pauses drawing on grid squares.

--- js/script.js
+++ js/script.js
@@ -25,6 +25,9 @@ function drawOnHover(){
                 squareGrid[i].style.transition = "all 125ms linear";
                 squareGrid[i].style.borderColor = "#848484";
             }
+            else if(e.altKey){
+                squareGrid[i].style.cursor = "pointer";
+            }
             else{
                 squareGrid[i].style.backgroundColor = "#484848";
                 squareGrid[i].style.transition = "all 125ms linear";
